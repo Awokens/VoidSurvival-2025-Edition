6ef44f553075bcc95e57d6ee48457b0884aee344
Awokens <35590883+Awokens@users.noreply.github.com>
2025-04-13 21:34:08 -0700

    Add /ping command

--- src/main/java/com/github/voidSurvival2025/Commands/PingCmd.java
+++ src/main/java/com/github/voidSurvival2025/Commands/PingCmd.java
@@ -1,6 +1,28 @@
 package com.github.voidSurvival2025.Commands;
 
+import com.github.voidSurvival2025.VoidSurvival2025;
+import dev.jorel.commandapi.CommandAPICommand;
+import dev.jorel.commandapi.arguments.PlayerArgument;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import org.bukkit.entity.Player;
+
 public class PingCmd {
 
+    private VoidSurvival2025 plugin;
+    public PingCmd(VoidSurvival2025 plugin) {
+        new CommandAPICommand("ping")
+                .withOptionalArguments(new PlayerArgument("target"))
+                .executesPlayer((player, args) -> {
 
+
+                    Player target = (Player) args.getOptional("target").orElse(player);
+
+                    int ping = target.getPing();
+
+                    player.sendMessage(MiniMessage.miniMessage().deserialize(
+                            target.getName() + "'s ping is <green>" + ping + " ms"
+                    ));
+
+                }).register();
+    }
 }
