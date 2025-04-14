ecbb960b6c0e31d9caab1dfadd57cf17da128fe6
Awokens <35590883+Awokens@users.noreply.github.com>
2025-04-13 21:38:58 -0700

    Added a WHATISTHISSERVER!???! command.

---
+++ src/main/java/com/github/voidSurvival2025/Commands/WhatIsThisServerCmd.java
@@ -0,0 +1,31 @@
+package com.github.voidSurvival2025.Commands;
+
+import com.github.voidSurvival2025.VoidSurvival2025;
+import dev.jorel.commandapi.CommandAPICommand;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+
+public class WhatIsThisServerCmd {
+
+    public WhatIsThisServerCmd() {
+        new CommandAPICommand("whatisthisserver?")
+                .executesPlayer((player, args) -> {
+
+
+                    player.sendMessage(MiniMessage.miniMessage().deserialize(
+                            """
+                                    
+                                    <yellow>What is this server?</yellow>
+                                    This is essentially <color:#ffa600>skyblock</color> mashed with\s
+                                    a <color:#ffa600>border</color> and a platform in the middle.\s
+                                    And you are given <color:#ffa600>wood</color>, <color:#ffa600>cobble</color> and some\s
+                                    <color:#ffa600>iron nuggets</color> <color:#ffee00>randomly every 10 seconds</color>.\s
+                                    
+                                    → Find out more with <green>/wiki</green>
+                                    We also have a community discord.\s
+                                    → <color:#308aff><click:open_url:'https://discord.gg/FJKxwNGdVC'>https://discord.gg/FJKxwNGdVC</click></color>
+                                    
+                                    """
+                    ));
+                }).register();
+    }
+}
