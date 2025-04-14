f5abf8246cb98d7acee7947949f82ea4f7d76c6a
Awokens <35590883+Awokens@users.noreply.github.com>
2025-04-13 21:33:47 -0700

    Made sure portals aren't created, rather teleported to bedrock platform.

---
+++ src/main/java/com/github/voidSurvival2025/Features/Player/NetherPortalEnter.java
@@ -0,0 +1,36 @@
+package com.github.voidSurvival2025.Features.Player;
+
+import com.github.voidSurvival2025.Manager.SpawnPointManager;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerPortalEvent;
+import org.bukkit.event.player.PlayerTeleportEvent;
+import org.bukkit.event.world.PortalCreateEvent;
+
+public class NetherPortalEnter implements Listener {
+
+    @EventHandler
+    public void portal(PlayerPortalEvent event) {
+        if (event.getCause() == PlayerTeleportEvent.TeleportCause.END_PORTAL) return;
+
+        event.setCancelled(true);
+        Player player = event.getPlayer();
+
+        Location teleportTo = switch (player.getWorld().getName()) {
+            case "world_nether" -> SpawnPointManager.getWorldSpawn();
+            case "world" -> SpawnPointManager.getNetherSpawn();
+            default -> throw new IllegalStateException("Unexpected value: " + player.getWorld().getName());
+        };
+        player.teleportAsync(teleportTo);
+    }
+
+    @EventHandler
+    public void portalCreate(PortalCreateEvent event) {
+
+        if (event.getReason() == PortalCreateEvent.CreateReason.FIRE) return; // nether
+
+        event.setCancelled(true);
+    }
+}
